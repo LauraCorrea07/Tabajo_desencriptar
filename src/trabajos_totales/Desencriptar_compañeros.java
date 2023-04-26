d828a66c684fa1fb982438b4a3170e39c63da52a
Laura correa <laura.correa07@uptc.edu.co>
2023-04-25 20:28:50 -0500

    ejercicio de camargo

--- src/trabajos_totales/Desencriptar_compañeros.java
+++ src/trabajos_totales/Desencriptar_compañeros.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Desencriptar_compañeros {
 
 	public static void main(String[] args) {
+		//SAMUEL CASTRO
 		Scanner teclado = new Scanner (System.in);							
 				System.out.println("Ingrese la palabra o frase");				
 				String palabra = teclado.nextLine();				
@@ -203,7 +204,7 @@ public class Desencriptar_compañeros {
 	
 		//SAMUEL CAMARGO 
 				System.out.println("Ingrese la palabra");
-				String palabraE = teclado.nextLine(); 
+				String palabraE = teclado.nextLine();
 				String encriptarPalabra = ""; //se inicializa una variable para contener el resultado       
 				String[] abecedario = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}; // se inicializa un vector para contener las letras del abecedario
 				String[] deletreo = new String [palabraE.length()]; // se inicializa un vector para almacenar la palabra a encriptar
@@ -214,18 +215,18 @@ public class Desencriptar_compañeros {
 					String deletras = deletreo[j]; // se inicializa una variable que almacene el vector de deletreo                        
 					for(int i=0; i<26;i++){ // para i = o mientras i sea menor que 26 aumentar i en 1                
 						String letras = abecedario[i];  // se inicializa una variable que almacene las letras del abecedario
-						int condicion = i - palabraE.length()*2;  /*se inicializa una variable que almacena las condiciones de encriptacion 
+						int condicion = i + palabraE.length()*2;  /*se inicializa una variable que almacena las condiciones de encriptacion 
 		                                                    que es restar el numero de espacios del largo de la palabra y se multiplica ese valor en 2 */ 
-						if(condicion<0){  // se crea una condicion para evitar que la encriptacion se desborde del abecedario
-							condicion=condicion+abecedario.length;
+						if(condicion > abecedario.length-1){  // se crea una condicion para evitar que la encriptacion se desborde del abecedario
+							condicion=condicion-abecedario.length;
 						}
 						if (deletras.contains(letras) == true ){ // se crea la condicion para encriptar la palabra                        
 							encriptarPalabra = encriptarPalabra +  abecedario[condicion];                
 						}
 					}
 				}
-				System.out.println(encriptarPalabra);  
-			   
+				System.out.println(encriptarPalabra);   
+			  
 
 	}
 }
